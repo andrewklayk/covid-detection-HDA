c0b907322d7c3b6ceb03fa42189c3567f747f4f9
andrewklayk <andrew.klyachkin@gmail.com>
2022-02-08 17:13:58 +0100

    initial commit

---
+++ gpu_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+print(tf.config.list_physical_devices(
+    device_type=None
+))
+
